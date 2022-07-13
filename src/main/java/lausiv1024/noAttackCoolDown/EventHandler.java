2df8dbf9a7459fd6d6a4def9a02cbcf86a1292c5
Lausiv1024 <75715137+Lausiv1024@users.noreply.github.com>
2022-07-13 20:28:54 +0900

    Completed For 1.18

---
+++ src/main/java/lausiv1024/noAttackCoolDown/EventHandler.java
@@ -0,0 +1,13 @@
+package lausiv1024.noAttackCoolDown;
+
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class EventHandler {
+    @SubscribeEvent
+    public static void playerEvent(TickEvent.PlayerTickEvent event){
+        event.player.attackStrengthTicker = 256;
+    }
+}
